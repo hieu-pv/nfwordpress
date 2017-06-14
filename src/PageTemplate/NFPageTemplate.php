d291b2fdc7af42796f1e1f0758a2cd3d05c3b3e5
Hieupv <hieu-pv@users.noreply.github.com>
2017-06-14 08:23:14 +0700

    Create NFPageTemplate.php

--- src/PageTemplate/NFPageTemplate.php
+++ src/PageTemplate/NFPageTemplate.php
@@ -40,8 +40,26 @@ class NFPageTemplate
     {
         $this->templates = array();
 
+        /************ Old code **************/
+        /* // Add a filter to the attributes metabox to inject template into the cache.
+        /* add_filter('page_attributes_dropdown_pages_args', [$this, 'register_project_templates']);
+        /************ End Old Code **********/
+        
+        /************ New code **************/
         // Add a filter to the attributes metabox to inject template into the cache.
-        add_filter('page_attributes_dropdown_pages_args', [$this, 'register_project_templates']);
+		if ( version_compare( floatval( get_bloginfo( 'version' ) ), '4.7', '<' ) ) {
+			// 4.6 and older
+			add_filter(
+				'page_attributes_dropdown_pages_args',
+				array( $this, 'register_project_templates' )
+			);
+		} else {
+			// Add a filter to the wp 4.7 version attributes metabox
+			add_filter(
+				'theme_page_templates', array( $this, 'add_new_template' )
+			);
+		}
+		/************* End New Code ***********/
 
         // Add a filter to the save post to inject out template into the page cache
         add_filter('wp_insert_post_data', [$this, 'register_project_templates']);
@@ -53,6 +71,16 @@ class NFPageTemplate
         // Add your templates to this array.
         $this->templates = [__FILE__=> $this->name];
     }
+    /*************** New code added ************/
+    /**
+	 * Adds our template to the page dropdown for v4.7+
+	 *
+	 */
+	public function add_new_template( $posts_templates ) {
+		$posts_templates = array_merge( $posts_templates, $this->templates );
+		return $posts_templates;
+	}
+	/*************** End new code added **********/
 
     /**
      * Adds our template to the pages cache in order to trick WordPress
